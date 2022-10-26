c363188b2e6787b79e97f27c7d5b0cb9e2ad0871
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-10-26 14:11:35 +0900

    Update stepping_stone.py

--- programmers/binary_search/stepping_stone/stepping_stone.py
+++ programmers/binary_search/stepping_stone/stepping_stone.py
@@ -1,25 +1,43 @@
+
 def solution(distance, rocks, n):
-    answer = 0
-    rocks.append(distance)
+    max_of_min = 0
     rocks.sort()
-    offset = [rocks[0]]
+    rocks.append(distance)
+    
+    # 출발지점과 첫 바위에 대한 간격 추가
+    intervals = [rocks[0]]
+    
+    # 바위 간의 간격 저장
     for i in range(1, len(rocks)):
-        offset.append(rocks[i]-rocks[i-1])
-    m = 1
-    M = distance
-    while m < M:
-        cnt = 0
-        medium = (m+M)//2
-        copied = offset[::]
-        for i in range(len(copied)-1):
-            if copied[i] < medium:
-                copied[i+1] += copied[i]
-                cnt += 1
-        if copied[-1] < medium:
-            cnt += 1
-        if cnt <= n:
-            answer = max(answer, medium)
-            m = medium + 1
+        intervals.append(rocks[i] - rocks[i-1])
+        
+    # 최소간격의 최소 경우와 최대 경우
+    l, r = 0, distance
+    
+    # 바위 제거 수와 최소간격은 비례한다는 점을 두고,
+    # 최소 간격의 모든 경우 중 n개 제거로 가능한 경우를 찾는다.
+    while l <= r:
+        min_interval = (l+r) // 2
+        remove = 0
+        
+        # 목표 최소간격을 넘기 위해 간격을 더해간다.
+        collect = 0
+        for i in range(len(intervals)):
+            collect += intervals[i]
+            
+            # 아직 최소간격에 도달하지 못한다면, 바위를 지우고 간격을 더한다.
+            if collect < min_interval:
+                remove += 1
+            else:
+                collect = 0
+        
+        # n개 제거만으로 이 최소간격(min_interval)은 만들 수 없으니, 목표를 낮춘다.
+        if remove > n:
+            r = min_interval - 1
+        # n개 이하로 이 최소간격(min_interval)을 만들 수 있다면,
+        # 일단 정답에 저장 후, 목표를 더욱 높여본다.
         else:
-            M = medium
-    return answer
+            max_of_min = max(max_of_min, min_interval)
+            l = min_interval + 1
+    
+    return max_of_min
