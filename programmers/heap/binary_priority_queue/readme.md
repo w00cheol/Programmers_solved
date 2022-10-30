20e8578fa0b757252732606654468d187f6248e8
w00cheol <kw8384@gmail.com>
2022-10-30 18:53:18 +0900

    refactor h_index

--- programmers/heap/binary_priority_queue/readme.md
+++ programmers/heap/binary_priority_queue/readme.md
@@ -1,4 +1,4 @@
-### 디스크 컨트롤러
+### 이중우선순위큐
 
 ***
 [출처](https://programmers.co.kr/learn/courses/30/lessons/42628)
--- programmers/sort/h_index/h_index.py
+++ programmers/sort/h_index/h_index.py
@@ -1,11 +1,8 @@
 def solution(citations):
-    answer = i = 0
-    citations.sort()
-    if citations[-1] == 0:
-        return 0
-    while citations[i] <= len(citations[i:]):
-        answer = citations[i]
-        i += 1
-    for j in range(answer+1, citations[i]):
-        if j <= len(citations[i:]): answer += 1
-        else: return answer
+    citations.sort(reverse = True)
+    
+    for i in range(len(citations)):
+        if i+1 > citations[i]:
+            return i
+        
+    return len(citations)
