dc513e56bf88706e74cbafb9169d833d06635a3d
w00cheol <kw8384@gmail.com>
2022-11-06 22:50:02 +0900

    REFACTOR trucks

--- programmers/stack_queue/trucks/trucks.py
+++ programmers/stack_queue/trucks/trucks.py
@@ -1,21 +1,3 @@
 def solution(bridge_length, weight, truck_weights):
-    time = 1
-    cnt = 0
-    onBridge = [0]
-    howLong = {}
-    trucks = list(enumerate(truck_weights))
-    del trucks[0]
-    lenT = len(truck_weights)
-    for i in range(lenT): howLong[i] = 0
-    while trucks:
-        sum = 0
-        time += 1
-        for i in onBridge: howLong[i] += 1
-        if howLong[onBridge[0]] == bridge_length:
-            del onBridge[0]
-            cnt += 1
-        for i in onBridge: sum+= truck_weights[i]
-        if sum+trucks[0][1] <= weight:
-            onBridge.append(trucks[0][0])
-            del trucks[0]
-    return time+bridge_length
+    answer = 0
+    return answer
